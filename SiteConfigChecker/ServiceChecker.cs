7aa7836380cc452c10e0f098007e525622d2a8f6
Oleksandr Vyshnevskyi <mrrgoodcat@gmail.com>
2019-07-15 20:37:18 +0300

    Windows Service checker

---
+++ SiteConfigChecker/ServiceChecker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace SiteConfigChecker
+{
+    internal class ServiceChecker
+    {
+        internal ServiceStatusEnum Check(string serviceDisplayName, string serverLocation = null)
+        {
+            if (serverLocation == null)
+            {
+                serverLocation = Environment.MachineName;
+            }
+            ServiceStatusEnum serviceStatus = ServiceStatusEnum.Undefined;
+
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (var service in services)
+            {
+                if (service.DisplayName == serviceDisplayName)
+                {
+                    serviceStatus = Parse(service.Status);
+                }
+            }
+            return serviceStatus;
+        }
+
+        private ServiceStatusEnum Parse(ServiceControllerStatus status)
+        {
+            switch (status)
+            {
+                case ServiceControllerStatus.ContinuePending:
+                    {
+                        return ServiceStatusEnum.ContinuePending;
+                    }
+                case ServiceControllerStatus.Paused:
+                    {
+                        return ServiceStatusEnum.Paused;
+                    }
+                case ServiceControllerStatus.PausePending:
+                    {
+                        return ServiceStatusEnum.PausePending;
+                    }
+                case ServiceControllerStatus.Running:
+                    {
+                        return ServiceStatusEnum.Running;
+                    }
+                case ServiceControllerStatus.StartPending:
+                    {
+                        return ServiceStatusEnum.StartPending;
+                    }
+                case ServiceControllerStatus.Stopped:
+                    {
+                        return ServiceStatusEnum.Stopped;
+                    }
+                case ServiceControllerStatus.StopPending:
+                    {
+                        return ServiceStatusEnum.StopPending;
+                    }
+                default:
+                    {
+                        return ServiceStatusEnum.Undefined;
+                    }
+            }
+        }
+    }
+}
