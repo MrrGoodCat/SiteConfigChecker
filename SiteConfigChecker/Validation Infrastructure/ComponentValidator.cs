cf52a39933b2713606cd311f586c0dabfe4c7763
Vadym Pavliuk <Vadym.Pavliuk@nice.com>
2019-07-16 13:12:35 +0300

    + Validation base infra

---
+++ SiteConfigChecker/Validation Infrastructure/ComponentValidator.cs
@@ -0,0 +1,16 @@
+﻿using SiteConfigChecker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteConfigChecker.Validation_Infrastructure
+{
+    public interface ComponentValidator
+    {
+        List<Result> PublishValidationResults();
+
+        bool RunValidation();
+    }
+}
---
+++ SiteConfigChecker/Validation Infrastructure/ValidationResult.cs
@@ -0,0 +1,22 @@
+﻿using SiteConfigChecker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteConfigChecker.Validation_Infrastructure
+{
+    public class ValidationResult
+    {
+        public Result Result { get; set; }
+
+        List<KeyValuePair<string, string>> AdditionalInformation;
+
+        public ValidationResult(Result result, List<KeyValuePair<string, string>> additionalInfo)
+        {
+            Result = Result;
+            AdditionalInformation = additionalInfo;
+        }
+    }
+}
